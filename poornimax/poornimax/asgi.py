d538704e62c0e142add7232d9a87127c806a2e48
Vanshhsoni <sonivanshmaster@gmail.com>
2025-08-24 14:39:07 +0530

    Update asgi.py

--- poornimax/poornimax/asgi.py
+++ poornimax/poornimax/asgi.py
@@ -6,11 +6,11 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'poornimax.settings')
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import chat.routing  # safe, only routing patterns
 
 # 2️⃣ Set up ASGI application
 django_asgi_app = get_asgi_application()
 
+import chat.routing  # safe, only routing patterns
 application = ProtocolTypeRouter({
     'http': django_asgi_app,
     'websocket': AuthMiddlewareStack(
