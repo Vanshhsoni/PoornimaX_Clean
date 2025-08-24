ebe8fde5c0df42aa0cfc43dcf9d7b29e4322f4be
Vanshhsoni <sonivanshmaster@gmail.com>
2025-08-24 15:50:27 +0530

    Update settings.py chat 1

--- poornimax/poornimax/settings.py
+++ poornimax/poornimax/settings.py
@@ -81,6 +81,27 @@ DATABASES = {
     )
 }
 
+# Add to your settings.py
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO',
+    },
+    'loggers': {
+        'chat.consumers': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+    },
+}
 # Static files
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
