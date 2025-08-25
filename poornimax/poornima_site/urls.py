9f36b1c601bfe11d4510a144328ebab9645c3a38
Vanshhsoni <sonivanshmaster@gmail.com>
2025-08-25 14:12:28 +0530

    Update urls.py

--- poornimax/poornima_site/urls.py
+++ poornimax/poornima_site/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from . import views  # current app ke views
-from accounts import views as account_views  # accounts app ke views
+from . import views  # current app ccounts app ke views
 
 urlpatterns = [
     # Home page ko accounts ka login_signup bana do
-    path('', account_views.login_signup, name='home'),
+    path('', views.home, name='home'),
 ]
