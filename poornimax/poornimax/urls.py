746f3a13954a9cfc98f3cc4354dc8fd27403d34b
Vanshhsoni <sonivanshmaster@gmail.com>
2025-08-26 06:17:20 +0530

    Update urls.py

--- poornimax/poornimax/urls.py
+++ poornimax/poornimax/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from . import urls
+from django.views.generic import TemplateView  # 👈 add this
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -11,5 +11,9 @@ urlpatterns = [
     path('feed/', include('feed.urls')),
     path('chat/', include('chat.urls', namespace='chat')),
 
-    
+    # 👇 Google verification file (replace filename with your actual one)
+    path(
+        "google123456789abc.html",  
+        TemplateView.as_view(template_name="google123456789abc.html")
+    ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
