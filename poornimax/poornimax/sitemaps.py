8059e0ea6ccd8c724e85d0e86bd3c66d9c0b7cdf
Vanshhsoni <sonivanshmaster@gmail.com>
2025-08-26 08:50:55 +0530

    Update sitemaps.py

--- poornimax/poornimax/sitemaps.py
+++ poornimax/poornimax/sitemaps.py
@@ -1,1 +1,18 @@
+# sitemaps.py
+from django.contrib.sitemaps import Sitemap
+from django.urls import reverse
 
+class StaticSitemap(Sitemap):
+    changefreq = "daily"
+    priority = 0.8
+
+    def items(self):
+        # yahan apne url "name" dalne hain (not paths)
+        return [
+            "home",            # poornima_site.urls → home view
+            "login_signup",    # accounts.urls → signup page
+            "load_login",      # accounts.urls → login page
+        ]
+
+    def location(self, item):
+        return reverse(item)
